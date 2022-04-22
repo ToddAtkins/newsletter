220c7c0ad10b61667bfa29c788659dd261caf6d2
E. Todd Atkins <etoddatkins@ucsb.edu>
2022-04-22 09:57:46 -0700

    updated version

--- setup.py
+++ setup.py
@@ -1,28 +1,26 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 from setuptools import setup, find_packages
 
 setup(
     name = 'newsletter',
-    version = '1.1.0',
+    version = '3.1.0',
     description = 'simple newsletter generator',
     packages = find_packages(include=['publish']),
     install_requires = [
         'icalendar',
         'pytz',
         'python-twitter',
-	'requests',
+    	'requests',
         'tzlocal',
     ],
     namespace_packages = ['publish'],
-    entry_points = {
-        'console_scripts': [
-            'publish = publish.__main__:main',
-            'mycalendar = publish.mycalendar:main',
-            'quote = publish.quote:main',
-            'mytweets = publish.mytwitter:main',
-        ],
-    },
-    author = 'E. Todd Atkins',
+    scripts = [
+        'bin/publish',
+        'bin/events',
+        'bin/tweets',
+        'bin/quote-of-the-day'
+    ],
+    author = 'Todd Atkins',
     author_email = 'sbwino@gmail.com',
     license = 'BSD',
     url = 'https://github.com/ToddAtkins/newsletter',
